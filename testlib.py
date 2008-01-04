371c6436cbce6c67d70eb7e5a51daf04f5c27713
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2008-01-04 11:21:41 +0100

    override assertRaises to return the exception raised

--- testlib.py
+++ testlib.py
@@ -1101,6 +1101,30 @@ class TestCase(unittest.TestCase):
         self.assert_(isinstance(obj, klass), msg)
 
 
+    def failUnlessRaises(self, excClass, callableObj, *args, **kwargs):
+        """override default failUnlessRaise method to return the raised
+        exception instance.
+        
+        Fail unless an exception of class excClass is thrown
+        by callableObj when invoked with arguments args and keyword
+        arguments kwargs. If a different type of exception is
+        thrown, it will not be caught, and the test case will be
+        deemed to have suffered an error, exactly as for an
+        unexpected exception.
+        """
+        try:
+            callableObj(*args, **kwargs)
+        except excClass, exc:
+            return exc
+        else:
+            if hasattr(excClass, '__name__'):
+                excName = excClass.__name__
+            else:
+                excName = str(excClass)
+            raise self.failureException, "%s not raised" % excName
+
+    assertRaises = failUnlessRaises
+
 import doctest
 
 class SkippedSuite(unittest.TestSuite):
