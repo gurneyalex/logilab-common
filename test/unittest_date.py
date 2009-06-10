7f66ba01c3d47e9692c6f2ba44105c3f76df81c3
Graziella Toutoungis <graziella.toutoungis@logilab.fr>
2009-06-10 12:19:03 +0200

    remove duplicate test method

--- test/unittest_date.py
+++ test/unittest_date.py
@@ -30,7 +30,7 @@ class DateTC(TestCase):
         expected = [self.datecls(2000,1,31), self.datecls(2000,2,1), self.datecls(2000,2,2)]
         self.assertListEquals(r, expected)
 
-    def test_add_days_worked_with_mx(self):
+    def test_add_days_worked(self):
         add = add_days_worked
         # normal
         self.assertEquals(add(self.datecls(2008, 1, 3), 1), self.datecls(2008, 1, 4))
@@ -40,19 +40,7 @@ class DateTC(TestCase):
         self.assertEquals(add(self.datecls(2008, 1, 3), 8), self.datecls(2008, 1, 15))
         # skip holiday + week-end
         self.assertEquals(add(self.datecls(2008, 4, 30), 2), self.datecls(2008, 5, 5))
-
-    def test_add_days_worked_with_pydatetime(self):
-        add = add_days_worked
-        Date = datetime
-        # normal
-        self.assertEquals(add(self.datecls(2008, 1, 3), 1), self.datecls(2008, 1, 4))
-        # skip week-end
-        self.assertEquals(add(self.datecls(2008, 1, 3), 2), self.datecls(2008, 1, 7))
-        # skip 2 week-ends
-        self.assertEquals(add(self.datecls(2008, 1, 3), 8), self.datecls(2008, 1, 15))
-        # skip holiday + week-end
-        self.assertEquals(add(self.datecls(2008, 4, 30), 2), self.datecls(2008, 5, 5))
-
+        
     def test_get_national_holidays(self):
         holidays = get_national_holidays
         yield self.assertEquals, holidays(self.datecls(2008, 4, 29), self.datecls(2008, 5, 2)), \
